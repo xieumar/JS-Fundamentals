3230390474203367cd3743d31a28d872ba3976b0
Zainab Umar Ibrahim <xieumar@gmail.com>
2025-05-25 11:45:27 +0100

    Added 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+const languages = {
+    'c' :  'C is fun',
+    'python': 'Python is cool',
+    'javascript': 'JavaScript is amazing'
+} ;
+
+for ( let lang in languages ) {
+    console.log(languages[lang]);
+}
